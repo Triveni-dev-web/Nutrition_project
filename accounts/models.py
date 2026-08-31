6118e4eaf15876efb2db188468c4cea5388f1387
Triveni-dev-web <YOUR_GITHUB_EMAIL@gmail.com>
2026-08-31 23:50:20 +0530

    Complete Milestone 1 nutrition Project

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,51 @@
 from django.db import models
+
+
+# ================= FOOD ENTRY =================
+
 class FoodEntry(models.Model):
     date = models.DateField()
     meal_type = models.CharField(max_length=20)
     food_name = models.CharField(max_length=100)
     quantity = models.CharField(max_length=50)
 
+    calories = models.FloatField(default=0)
+    protein = models.FloatField(default=0)
+    carbohydrates = models.FloatField(default=0)
+
     def __str__(self):
         return f"{self.food_name} - {self.meal_type}"
-# Create your models here.
+
+
+# ================= SYMPTOM =================
+
+class Symptom(models.Model):
+    name = models.CharField(max_length=100)
+    severity = models.CharField(max_length=20)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.severity}"
+# ================= HEALTH PROFILE =================
+
+class HealthProfile(models.Model):
+
+    age = models.IntegerField()
+    gender = models.CharField(max_length=20)
+    height = models.FloatField()
+    weight = models.FloatField()
+
+    activity = models.CharField(max_length=30)
+    goal = models.CharField(max_length=30)
+    diet = models.CharField(max_length=30)
+
+    medical_conditions = models.TextField(
+        blank=True
+    )
+
+    dietary_restrictions = models.TextField(
+        blank=True
+    )
+
+    def __str__(self):
+        return f"Health Profile - {self.age}"    
--- nutrition_app/nutrition_app/settings.py
+++ nutrition_app/nutrition_app/settings.py
@@ -1,3 +1,6 @@
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 """
 Django settings for nutrition_app project.
 
@@ -115,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+USDA_API_KEY=os.getenv("USDA_API_KEY")
